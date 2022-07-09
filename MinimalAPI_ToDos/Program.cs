45537a70b8af9877e98e16b99e5ba1e8724073d3
Mariusz Nowak <m.nowak1219@gmail.com>
2022-07-09 17:04:22 +0200

    Add project files.

---
+++ MinimalAPI_ToDos/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IToDoService, ToDoService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapGet("/todos", ([FromServices] IToDoService service) => service.GetAll());
+app.MapGet("/todos/{id}", ([FromServices]IToDoService service, [FromRoute]Guid id) => service.GetById(id));
+app.MapPost("/todos", ([FromServices] IToDoService service, [FromBody] ToDo toDo) => service.Create(toDo));
+app.MapPut("/todos/{id}", ([FromServices] IToDoService service, [FromRoute] Guid id, [FromBody] ToDo toDo) => service.Update(toDo));
+app.MapDelete("/todos/{id}", ([FromServices] IToDoService service, [FromRoute] Guid id) => service.Delete(id));
+app.Run();
---
+++ MinimalAPI_ToDos/Usings.cs
@@ -0,0 +1,3 @@
+﻿global using MinimalAPI_ToDos.Entities;
+global using MinimalAPI_ToDos.Services;
+global using Microsoft.AspNetCore.Mvc;
